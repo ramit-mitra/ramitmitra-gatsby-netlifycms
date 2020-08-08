a7ebb8b90930ca421e90b43927b0676f53d5c6c4
Ramit Mitra <ramit.mitra@gmail.com>
2020-08-09 01:22:37 +0530

    Adding react social icons
    https://www.npmjs.com/package/react-social-icons

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link, useStaticQuery, graphql } from "gatsby";
 import Navigation from "../components/navigation";
+import { SocialIcon } from "react-social-icons";
 import "prismjs/themes/prism-okaidia.css";
 
 export default ({ children }) => {
@@ -20,6 +21,16 @@ export default ({ children }) => {
       <header className="site-header">
         <div className="site-title">
           <Link to="/">{data.site.siteMetadata.title}</Link>
+          <br />
+          <SocialIcon
+            url="https://twitter.com/ramit_mitra"
+            style={{ height: 25, width: 25 }}
+          />
+          &nbsp;
+          <SocialIcon
+            url="https://github.com/ramit-mitra"
+            style={{ height: 25, width: 25 }}
+          />
         </div>
         <Navigation />
       </header>
