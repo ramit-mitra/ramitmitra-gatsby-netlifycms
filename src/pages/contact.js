b9f68881616ab1a5d343f2767a861390512751f4
Ramit Mitra <ramit.mitra@gmail.com>
2020-08-09 01:46:07 +0530

    Updating contact form to submit via netlify form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -27,24 +27,25 @@ const ContactPage = ({ data: { site } }) => {
         <div>
           <form
             className="form-container"
-            action="https://sendmail.w3layouts.com/SubmitContactForm"
-            method="post"
+            name="contact"
+            method="POST"
+            data-netlify="true"
           >
             <div>
-              <label htmlFor="w3lName">Name</label>
-              <input type="text" name="w3lName" id="w3lName" />
+              <label htmlFor="name">Name</label>
+              <input type="text" name="name" id="name" />
             </div>
             <div>
-              <label htmlFor="w3lSender">Email</label>
-              <input type="email" name="w3lSender" id="w3lSender" />
+              <label htmlFor="email">Email</label>
+              <input type="email" name="email" id="email" />
             </div>
             <div>
-              <label htmlFor="w3lSubject">Subject</label>
-              <input type="text" name="w3lSubject" id="w3lSubject" />
+              <label htmlFor="subject">Subject</label>
+              <input type="text" name="subject" id="subject" />
             </div>
             <div>
-              <label htmlFor="w3lMessage">Message</label>
-              <textarea name="w3lMessage" id="w3lMessage"></textarea>
+              <label htmlFor="message">Message</label>
+              <textarea name="message" id="message"></textarea>
             </div>
             <div style={{ display: "flex", justifyContent: "flex-end" }}>
               <input
